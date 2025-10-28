ddf8c01ec8d00ff0652b3f36e5c2bbefb90a3d62
hot666666 <hotcs6@naver.com>
2025-10-28 15:01:55 +0900

    Initial Commit

---
+++ SimpleBlockExample/SimpleBlockExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBlockExampleApp.swift
+//  SimpleBlockExample
+//
+//  Created by hs on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBlockExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
